957baf52b6ad5e0ad00531336ad900f32ca7336d
Sven Büsing <s.buesing@babiel.com>
2016-08-03 11:49:56 +0200

    Disable Tower Notifiy

--- main.js
+++ main.js
@@ -133,14 +133,14 @@ module.exports.loop = function () {
     if (!(name == undefined) && !(name < 0)) {
         console.log("Spawned new creep: " + name);
     }
-    
+
     function defendRoom(roomName) {
-    
+
         var hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
-    
+
         if(hostiles.length > 0) {
             var username = hostiles[0].owner.username;
-            Game.notify(`User ${username} spotted in room ${roomName}`);
+            //Game.notify(`User ${username} spotted in room ${roomName}`);
             var towers = Game.rooms[roomName].find(
                 FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
             towers.forEach(tower => tower.attack(hostiles[0]));
